009e69cb25b46e27cd8c6bc49191b09a053ad6ec
Nathan Duncan <nduncan.au@gmail.com>
2018-01-19 14:31:35 +1100

    Dark sky calculator now working.

--- dark_skies.py
+++ dark_skies.py
@@ -1,35 +1,32 @@
 # a basic program that determines the best nights for stargazing based on the
 # moon's phase and rise/set times
-
-
-
 import ephem
 import pandas
-import time
 from datetime import datetime
 from datetime import timedelta
 import pytz
 
-
+# location parameters
 name = "Albury"
-# region = "Australia"
 lat = -36.07
 lon = 146.91
 timezone = "Australia/Sydney"
 # elev = 160
+
+# observer initialisation
 observer = ephem.Observer()
 observer.name = name
 observer.lat = str(lat)
 observer.lon = str(lon)
 start_date = datetime(2018,8,1)
-end_date = datetime(2018,8,31)
-# list of dates in 2018
-# p_dates = pandas.date_range(start=start_date, end=end_date, timezone=timezone).tolist()
+end_date = datetime(2018,10,31)
 
-# each night should be attributed to a single date
-# possibly change each day to the 24 hour period between noon?
 
+# return a date range including UTC offset for each date
 def date_range(start_date=start_date, end_date=end_date, time_adjust=0):
+
+    if not type(time_adjust) == timedelta:
+        time_adjust = timedelta(hours=time_adjust)
 
     p_dates = pandas.date_range(start=start_date, end=end_date).tolist()
     dates = []
@@ -46,7 +43,7 @@ def date_range(start_date=start_date, end_date=end_date, time_adjust=0):
     return dates
 
 
-def moon_phases(dates):
+def moon_phases(dates=date_range()):
 
     # define location parameters
     observer = ephem.Observer()
@@ -71,95 +68,127 @@ def moon_phases(dates):
         print "{} Next rising: {} {} {}%".format(day, next_rising, phase, int(round(moon.phase)))
 
 
-def transit(bodies=[ephem.Sun(), ephem.Moon()], start_date=start_date, end_date=end_date, time_adjust=timedelta(hours=12)):
+def dark_skies(start_date=start_date, end_date=end_date, time_adjust=12, horizon=0):
 
+    # get the date range
     dates = date_range(start_date, end_date, time_adjust)
+    # establish bodies
+    bodies = { 'sun': ephem.Sun(), 'moon': ephem.Moon() }
 
-    # define location parameters
-    observer = ephem.Observer()
-    observer.name = name
-    observer.lat = str(lat)
-    observer.lon = str(lon)
+    # # define location parameters
+    # observer = ephem.Observer()
+    # observer.name = name
+    # observer.lat = str(lat)
+    # observer.lon = str(lon)
 
+    # use the list of keys (dates) so we can call the transit_details items in order
     dates_order = [ date[0] for date in dates ]
-    transit_details = { date: [utc_offset, []] for date, utc_offset in dates }
+    # create transit_details structure and fill with what we know
+    transit_details = { date: [utc_offset, {'sun': None, 'moon': None }, []] for date, utc_offset in dates }
 
     for d in dates_order:
-
         for body in bodies:
+
             # get the next rise & set times
-            rising_local, setting_local = rise_and_set(observer, body, d, transit_details[d][0])
+            rising_local, setting_local = rise_and_set(bodies[body], d, transit_details[d][0])
             # current date +1 day
             d_next = d + timedelta(days=1)
 
-            # make sure not out of our date range and attempting to assign a value to a non-existant key
+            # check whether body is above the horizon at start of day
+            start_above = above_horizon(bodies[body], d, transit_details[d][0])
+            transit_details[d][1][body] = start_above
+
+            # figure out which day our event falls within and apply it to the corresponding transit_details value
+            # rising
             if d <= rising_local < d_next:
-                transit_details[d][1].append([rising_local, 'rising', body.name])
+                transit_details[d][2].append([rising_local, True, bodies[body].name])
             elif d_next in dates_order:
-                transit_details[d_next][1].append([rising_local, 'rising', body.name])
+                transit_details[d_next][2].append([rising_local, True, bodies[body].name])
 
+            # setting
             if d <= setting_local < d_next:
-                transit_details[d][1].append([setting_local, 'setting', body.name])
+                transit_details[d][2].append([setting_local, False, bodies[body].name])
             elif d_next in dates_order:
-                transit_details[d_next][1].append([setting_local, 'setting', body.name])
+                transit_details[d_next][2].append([setting_local, False, bodies[body].name])
 
+    # order the dates within each transit_details entry
     for t in transit_details:
-        transit_details[t][1].sort()
+        transit_details[t][2].sort()
 
+    # determine which character to return to symbolise current sun & moon position
+    def return_chr(state):
+        sun_pos, moon_pos = state
+        if sun_pos and moon_pos:
+            return chr(219)
+        elif sun_pos and not moon_pos:
+            return chr(178)
+        elif not sun_pos and moon_pos:
+            return chr(176)
+        else:
+            return " "
 
+    # change the current state based on what's above the horizon
+    def change_state(current_state, change, body):
+        if body == 'Sun':
+            current_state[0] = change
+            return current_state
+        else:
+            current_state[1] = change
+            return current_state
+
+    # print variables
+    day_length = 86400
+    print_length = 96
+
+    # header details
+    header = "Dark Skies: {} >> {}".format(dates_order[0].date(), dates_order[-1].date())
+    header = "-"*(print_length/2-18)+header+"-"*(print_length/2-18)
+    print header
+    # time markers
+    times = [ str(x) if len(str(x)) == 2 else '0'+str(x) for x in range(12,25)+range(1,12) ]
+    time_markers = ""
+    for t in times:
+        time_markers += "|"+t+" "*((print_length/24)-3)
+    print time_markers
+
+    # go through each date
     for d in dates_order:
         to_print = ""
-        to_print+=str(d)+"  "
-        for event in transit_details[d][1]:
-            to_print+=str(event[0])+"  "
+
+        # get current sun & moon positions
+        sun_pos = transit_details[d][1]['sun']
+        moon_pos = transit_details[d][1]['moon']
+
+        # current state and time
+        c_state = [sun_pos, moon_pos]
+        c_chr = return_chr(c_state)
+        c_time = d
+
+        # iterate over events for the day
+        for event in transit_details[d][2]:
+            # next event time
+            n_time = event[0]
+            # next event state
+            n_state = change_state(c_state, event[1], event[2])
+            # time between previous event and next event
+            length = n_time - c_time
+            # length of string to add to to_print
+            length = int(round(length.total_seconds()/day_length*print_length, 0))
+            to_print += c_chr*length
+
+            # apply next variables to current variables to prepare for next iteration
+            c_state = n_state
+            c_chr = return_chr(c_state)
+            c_time = n_time
+
+        # finalise to_print and print it
+        to_print += c_chr*(print_length-len(to_print))+" >> "+str(d.date())
+
         print to_print
 
-    # return transit_details
 
-    # print " 1 1 1 1 1 1 1 2 2 2 2 2 0 0 0 0 0 0 0 0 0 1 1 1"
-    # print " 3 4 5 6 7 8 9 0 1 2 3 4 1 2 3 4 5 6 7 8 9 0 1 2"
-    # for d in dates_order:
-    #     events = transit_details[d][1]
-    #     events_count = len(events)
-    #     dn = chr(176)
-    #     up = chr(219)
-    #     result = ""
-    #     start = d
-    #
-    #     # start_len = transit[d][1][0][0] - d
-    #     # start_len = int(start_len.total_seconds())/1800
-    #     #
-    #     # if transit[d][1][0][1] == 'rising':
-    #     #     result = result + dn * start_len
-    #     # else:
-    #     #     result = result + up * start_len
-    #     for event in events:
-    #         before = up if event[1] == 'setting' else dn
-    #         length = event[0] - start
-    #         length = int(length.total_seconds())
-    #         result += before*(length/450)
-    #         start = event[0]
-    #
-    #     # tomorrow = d + timedelta(days=1)
-    #     # remaining = tomorrow - event[0]
-    #     # remaining = int(remaining.total_seconds())
-    #     # result += before*(remaining/1800)
-    #     before = up if before == dn else dn
-    #     result += before*(192-len(result))
-    #     result += " "+d.strftime("%Y-%m-%d")
-
-
-        # print result
-
-        # transit = { date.to_pydatetime(): [utc_offset, []] for date, utc_offset in dates }
-        # for event in transit[d]:
-        #     if event[1] == 'rising':
-        #         result = result +
-
-    return dates, transit_details
-
-
-def rise_and_set(observer, body, date, utc_offset):
+# return the next rise and set times
+def rise_and_set(body, date, utc_offset):
 
     # convert search time (at local) to UTC time
     observer.date = date - utc_offset
@@ -171,49 +200,28 @@ def rise_and_set(observer, body, date, utc_offset):
     # utc time of event
     rising_utc = ephem.Date(rising).datetime()
     setting_utc = ephem.Date(setting).datetime()
-    # print "utc rise: "+str(rising_utc)
-    # print "utc  set: "+str(setting_utc)
 
-    #convert to local time (and remove any time values < second)
+    # convert to local time (and remove any sub-second information)
     rising_local = rising_utc + utc_offset
     rising_local = datetime(rising_local.year, rising_local.month, rising_local.day, rising_local.hour, rising_local.minute)
     setting_local = setting_utc + utc_offset
     setting_local = datetime(setting_local.year, setting_local.month, setting_local.day, setting_local.hour, setting_local.minute)
 
-
-    # print date, rising_local, setting_local
-
     return rising_local, setting_local
 
+# check to see whether a body is above the horizon
+def above_horizon(body, date, utc_offset):
+
+    observer.date = date - utc_offset
+    body.compute(observer)
+
+    if body.alt > 0:
+        return True
+
+    return False
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# def event_times(observer, event, date):
-#
-#     return
-#
-#
-#
-# def dark_skies(dates=date_range(), horizon=0):
-#
-#     return
-#
 
 ## notes
 # refer to https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for possible tz variables
